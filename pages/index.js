160222010f2996fdab8176b0d895853d0b85e8fe
Amaraa <amarbat@1234.mn>
2021-01-27 17:36:11 +0800

    Анхны аппаа хадгалж байнаа

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,52 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+import Player from "../components/Player";
+import PlayerCSS from "../components/PlayerCSS";
 
 export default function Home() {
+  const videoJsOptions = {
+    techOrder: ["youtube"],
+    autoplay: true,
+    controls: true,
+    sources: [
+      {
+        src: "https://www.youtube.com/watch?v=dGzdZmcTwDY",
+        type: "video/youtube",
+      },
+    ],
+  };
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Миний NextJS сайт</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Тавтай морил : <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Энэхүү сайтыг
+          <code className={styles.code}>https://1234.mn/course/109</code>{" "}
+          хичээлээр бүтээв
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>Миний тухай &rarr;</h3>
+            <p>Би Next.js, React, Javascript технологиудад дуртай!</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h3>Хобби &rarr;</h3>
+            <p>Ном унших, кино үзэх, хөдөө аялах дуртай</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <Player {...videoJsOptions} />
         </div>
       </main>
 
@@ -56,10 +56,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Технологи{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
